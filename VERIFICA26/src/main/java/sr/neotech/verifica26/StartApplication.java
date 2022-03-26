86b34fb707d19c2359edbb11e769eb6364f16006
Federico <f.luchetti@neotech.srl>
2022-03-26 10:46:44 +0100

    MOFUNZIONA

--- VERIFICA26/src/main/java/sr/neotech/verifica26/StartApplication.java
+++ VERIFICA26/src/main/java/sr/neotech/verifica26/StartApplication.java
@@ -1,9 +1,12 @@
 package sr.neotech.verifica26;
 
+import org.kohsuke.randname.RandomNameGenerator;
+
 public class StartApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		RandomNameGenerator rnd = new RandomNameGenerator(0);
 		for (int i=0; i<10; i++)
 		    System.out.println(rnd.next());
 	}
