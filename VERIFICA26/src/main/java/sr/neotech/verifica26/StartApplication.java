246832c2afb74471fb52c1e6024081c4ee2783ec
Federico <f.luchetti@neotech.srl>
2022-03-26 10:39:40 +0100

    VERIFICA26

---
+++ VERIFICA26/src/main/java/sr/neotech/verifica26/StartApplication.java
@@ -0,0 +1,11 @@
+package sr.neotech.verifica26;
+
+public class StartApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0; i<10; i++)
+		    System.out.println(rnd.next());
+	}
+
+}
