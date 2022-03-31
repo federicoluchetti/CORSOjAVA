8c548f946a50d9b32b63c111b00503c9b5141705
Federico <f.luchetti@neotech.srl>
2022-04-01 00:14:03 +0200

    AGGIORNAMENTO AEROPORTO

--- Aeroporto/src/main/java/srl/neotech/aeroporto/Aeroporto.java
+++ Aeroporto/src/main/java/srl/neotech/aeroporto/Aeroporto.java
@@ -1,25 +1,37 @@
 package srl.neotech.aeroporto;
 
-import java.security.PublicKey;
+
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Set;
 
 public class Aeroporto {
 ArrayList <Aereo> Aerei = new ArrayList<Aereo>();
 ArrayList<Passeggero> Passeggeri= new ArrayList<Passeggero>();
-private int raggioDiAzione;
+
+Random r = new Random();
+int raggioDiAzione = r.nextInt(100) + 1;
 public int getRaggioDiAzione() {
 	return raggioDiAzione;
 }
 public void setRaggioDiAzione(int raggioDiAzione) {
-	this.raggioDiAzione = raggioDiAzione;
+	this.raggioDiAzione = raggioDiAzione ;
 
-
-if(raggioDiAzione<1&&raggioDiAzione>=100) {
-	Aereo aereoDecollo=new Aereo();
-public 
-
-}
+	if(raggioDiAzione>=1&&raggioDiAzione<=100) {
+		Aereo aereoDecollo=new Aereo();
+		Aereo aereoAtterraggio=new Aereo();
+		Passeggero passeggeroCheckin=new Passeggero();
+		Passeggero passeggeroCheckout=new Passeggero();
+aereoDecollo.getStato();equals(Stati.ATTERRATO);
+aereoDecollo.setStato(Stati.DECOLLATO);
+aereoAtterraggio.getStato();equals(Stati.IN_AVVICINAMENTO);
+aereoAtterraggio.setStato(Stati.ATTERRATO);
+passeggeroCheckin.getStatoPasseggero();equals(StatoPasseggero.CHECK_IN);
+passeggeroCheckin.setStatoPasseggero(StatoPasseggero.A_BORDO);
+passeggeroCheckout.getStatoPasseggero();equals(StatoPasseggero.CHECK_OUT);
+passeggeroCheckout.setStatoPasseggero(StatoPasseggero.A_TERRA);
+	}
+ 
 
 
 }
--- Aeroporto/src/main/java/srl/neotech/aeroporto/StatoPasseggero.java
+++ Aeroporto/src/main/java/srl/neotech/aeroporto/StatoPasseggero.java
@@ -3,5 +3,6 @@ package srl.neotech.aeroporto;
 public enum StatoPasseggero {
 CHECK_IN,
 A_BORDO,
-CHECK_OUT;
+CHECK_OUT,
+A_TERRA;
 }
